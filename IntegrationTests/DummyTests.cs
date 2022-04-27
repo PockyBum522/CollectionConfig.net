fe6cb017d8be21fdf9e9bc5d17ce329810a47b0e
David Sikes <Me@DSikes.com>
2022-04-27 09:25:44 -0400

    Initialized new project structure

---
+++ IntegrationTests/DummyTests.cs
@@ -0,0 +1,30 @@
+﻿using System.IO;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace CollectionConfig.net.IntegrationTests;
+
+public class DummyTests
+{
+    // private _sut;
+    //
+    // [SetUp]
+    // public void SetUp()
+    // {
+    //     _sut = new();
+    // }
+    //
+    // [TearDown]
+    // public void TearDown()
+    // {
+    //     _sut = null;
+    // }
+    //
+    // [Test]
+    // public void Sut_AfterTestSetup_ShouldNotBeNull()
+    // {
+    //     var result = _sut;
+    //
+    //     result.Should().NotBeNull();
+    // }
+}
---
+++ UnitTests/DummyTests.cs
@@ -0,0 +1,26 @@
+﻿namespace CollectionConfig.net.UnitTests;
+
+public class DummyTests
+{
+    // private _sut;
+    //
+    // [SetUp]
+    // public void SetUp()
+    // {
+    //     _sut = new();
+    // }
+    //
+    // [TearDown]
+    // public void TearDown()
+    // {
+    //     _sut = null;
+    // }
+    //
+    // [Test]
+    // public void Sut_AfterTestSetup_ShouldNotBeNull()
+    // {
+    //     var result = _sut;
+    //
+    //     result.Should().NotBeNull();
+    // }
+}
