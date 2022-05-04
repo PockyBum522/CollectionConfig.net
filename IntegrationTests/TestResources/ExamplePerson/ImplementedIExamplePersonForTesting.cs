909ef692f49b0687b27c985acd5d9c1a197f6048
DSikes <david@sikespc.com>
2022-05-04 17:13:36 -0400

    Create ImplementedIExamplePersonForTesting.cs

---
+++ IntegrationTests/TestResources/ExamplePerson/ImplementedIExamplePersonForTesting.cs
@@ -0,0 +1,12 @@
+﻿// namespace CollectionConfig.net.IntegrationTests.TestResources.ExamplePerson;
+//
+// /// <summary>
+// /// Example person implementation, used for testing
+// /// </summary>
+// public class ImplementedIExamplePersonForTesting : IExamplePerson
+// {
+//     public string Name { get; set; } = "";
+//     public int Age { get; set; }
+//     public double MilesRun { get; set; }
+//     public string PetsName { get; set; } = "";
+// }
